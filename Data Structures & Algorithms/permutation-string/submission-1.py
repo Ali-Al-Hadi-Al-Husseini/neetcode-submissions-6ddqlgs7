1c01adad32b8f94ed0bbbe187891a7007e1ed1c5
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-04-25 23:13:01 +0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        start, end = 0, len(s1)
+
+
+        while end <= len(s2):
+            print(s2[start:end])
+            if is_same(s1,s2[start:end]):
+                return True
+
+            start += 1 
+            end += 1 
+        
+        return False
+
+
+
+def is_same(s1, s2):
+    s1_arr = [0] * 27
+    s2_arr = [0] * 27
+
+    for idx in range(len(s1)):
+        s1_arr[ord(s1[idx]) - ord("a")] += 1 
+        s2_arr[ord(s2[idx]) - ord("a")] += 1 
+
+
+
+
+    return s1_arr == s2_arr
