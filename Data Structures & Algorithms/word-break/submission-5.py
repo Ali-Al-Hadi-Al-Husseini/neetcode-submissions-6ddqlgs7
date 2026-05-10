ef111191de76f96fdce46db1383c9b3f20ba8dad
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-05-10 06:07:36 +0300

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        wordDict.sort(key=lambda x:-len(x))
+
+        def helper(curr_word):
+            if curr_word == s:
+                return True
+            if len(curr_word) >= len(s):
+                return False 
+            if curr_word in memo:
+                return memo[curr_word]
+
+            for word in wordDict:
+                if curr_word + word not in s :
+                    continue
+                memo[curr_word + word] = helper(curr_word+word)
+                if memo[curr_word + word] == True:
+                    return True
+
+            return False
+        
+        return helper('')
