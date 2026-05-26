ab0c5d7e9d0769e0c4ba81e3dadb7147cbb865b9
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-05-27 02:10:00 +0300

    Add: perfect-squares - submission-3

---
+++ Data Structures & Algorithms/perfect-squares/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo= {0:0,1:1,2:2,3:3,4:1}
+        
+        def perfect_sqaure_numbers(num):
+            if num <= 0 :
+                return 0
+            if num in memo:
+                return memo[num]
+
+            min_numbers = float("inf")
+            for curr_square in range(get_closet_sqaure(num),0,-1):
+                # print(curr_square)
+                curr = perfect_sqaure_numbers(num - (curr_square**2))  +1
+                min_numbers = min(min_numbers,curr)
+
+            memo[num] = min_numbers
+            return min_numbers
+
+        return perfect_sqaure_numbers(n)
+
+
+
+def get_closet_sqaure(num):
+    curr = 1
+    while  curr ** 2 <= num:
+        curr  += 1
+    return (curr - 1 )
+
+
