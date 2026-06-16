3cfdd96a58ecb4c91be46b260c56b9e9361f3b2a
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-06-16 05:39:44 +0300

    Add: reconstruct-flight-path - submission-9

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj_list = defaultdict(list)
+        for tick in tickets:
+            adj_list[tick[0]].append(tick[1])
+        
+        for airport in adj_list:
+            adj_list[airport].sort(reverse=True)
+
+        path = []
+        def dfs(source):
+            nonlocal path
+
+            while adj_list[source]:
+                destination  = adj_list[source].pop()
+                dfs(destination)
+            path.append(source)
+
+            return False 
+        dfs("JFK")
+        return path[::-1]
+
+
+
+
