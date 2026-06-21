15af0a64d7ec2ff9aa9ad2a0225e657ecd2aa5a4
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-06-21 21:39:56 +0300

    Add: candy - submission-7

---
+++ Data Structures & Algorithms/candy/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        childs = len(ratings)
+        candies = [1] * childs
+
+        for idx in range(1,childs):
+            if ratings[idx] > ratings[idx - 1]:
+                candies[idx] = candies[idx-1] +1
+
+        for idx in range(childs-1,0,-1):
+            if ratings[idx -1 ] > ratings[idx]:
+                candies[idx-1] = max(candies[idx] +1,candies[idx-1])
+            # if ratings[idx] > ratings[idx - 1] :
+            #     candies[idx] = max(candies[idx-1] +1,candies[idx])
+
+        return sum(candies)
