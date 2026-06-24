c96e576c8a768d4956f2d42cbce4d776b2b17417
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-06-24 13:33:58 +0300

    Add: maximum-frequency-stack - submission-2

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-2.py
@@ -0,0 +1,27 @@
+class FreqStack:
+
+    def __init__(self):
+        self.stack_map = defaultdict(list)
+        self.freqs = defaultdict(int)
+
+    def push(self, val: int) -> None:
+        self.freqs[val] +=1 
+        self.stack_map[self.freqs[val]].append(val)
+
+
+    def pop(self) -> int:
+        max_freq = len(self.stack_map)
+        poped_val = self.stack_map[max_freq].pop()
+        self.freqs[poped_val] -= 1 
+        if  self.freqs[poped_val] == 0 :
+            del  self.freqs[poped_val]
+        if len(self.stack_map[max_freq]) ==0 :
+            del self.stack_map[max_freq]
+        return poped_val
+        
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
