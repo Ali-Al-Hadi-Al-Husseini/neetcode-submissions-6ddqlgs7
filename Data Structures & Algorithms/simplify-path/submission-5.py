57fc40d552361d582cd77c59df272cdd4faac5d9
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-08-17 10:32:22 +0300

    Add: simplify-path - submission-5

---
+++ Data Structures & Algorithms/simplify-path/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        res = []
+        paths = path.split("/")
+
+        for dir_name in paths:
+            if dir_name == "/":
+                continue 
+            if dir_name == "..":
+                if res:
+                    res.pop()
+            elif dir_name  and dir_name!= ".":
+                res.append(dir_name)
+        
+        return "/" + "/".join(res)
